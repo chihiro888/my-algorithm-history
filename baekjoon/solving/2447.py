3c7b24b0cbdd0168a3b892d9e2ac138e9d044e41
return-shell <93immm@naver.com>
2022-04-05 22:34:27 +0900

    feature: keep solving problem

---
+++ baekjoon/solving/2447.py
@@ -0,0 +1,32 @@
+import math
+
+def print_start_list(data):
+    for x in data:
+        p = ''
+        for n in x:
+            p += n
+        print(p)
+
+def get_star_list(data):
+    result = []
+    loop_cnt = 3
+    for n in range(loop_cnt):
+        for i in data:
+            if n == 1:
+                result.append(i + ([' '] * len(data[0])) + i)
+            else:
+                result.append(i * loop_cnt)
+    return result
+
+# k = [1, 2, 3, 4, 5, 6, 7]
+# N = [3, 9, 27, 81, 243, 729, 2187]          
+def main():
+    N = int(input())
+    k = int(math.log(N, 3))
+    data = [['*']]
+    for _ in range(1, k+1):
+        data = get_star_list(data)
+    print_start_list(data)
+    
+if __name__ == '__main__':
+    main()
