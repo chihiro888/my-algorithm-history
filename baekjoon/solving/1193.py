e3bf5a5e502b25d69df2c6703bdf38680255b71d
return-shell <93immm@naver.com>
2022-04-06 21:32:10 +0900

    feature: solve 1193

--- baekjoon/solving/1193.py
+++ baekjoon/solving/1193.py
@@ -6,27 +6,27 @@ col = 1
 up = False
 down = False
 
-for n in range(1, 10000000):
-    # move
-    if row == 1 and (not down):
-        col += 1
-        up = False
-        down = True
-    elif col == 1 and (not up):
-        row += 1
-        up = True
-        down = False
-    elif up:
-        row -= 1
-        col += 1
-    elif down:
-        row += 1
-        col -= 1
+# solve
+if X == 1:
+    print('1/1')
+else:
+    for n in range(1, 10000000):
+        # move
+        if row == 1 and (not down):
+            col += 1
+            up = False
+            down = True
+        elif col == 1 and (not up):
+            row += 1
+            up = True
+            down = False
+        elif up:
+            row -= 1
+            col += 1
+        elif down:
+            row += 1
+            col -= 1
 
-    # print solution
-    if X == 1:
-        print('1/1')
-        break
-    elif X == n+1:
-        print(f'{row}/{col}')
-        break
+        if X == n+1:
+            print(f'{row}/{col}')
+            break
