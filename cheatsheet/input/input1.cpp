2f9526da499cf697ce559a74c39a11779ad2e389
return-shell <93immm@naver.com>
2022-04-12 04:17:31 +0900

    feature: add chaetsheet

---
+++ cheatsheet/input/input1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+when input as int
+1 2
+*/
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b;
+    
+    return 0;
+}
---
+++ cheatsheet/input/input2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+when input as string
+AAA BBB
+*/
+int main() {
+    string a, b;
+
+    cin >> a >> b;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b;
+    
+    return 0;
+}
