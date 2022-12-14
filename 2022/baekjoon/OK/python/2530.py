8f3b7ea23f7ac88d3337c27f2311ea6587429a50
chihiro888 <93immm@naver.com>
2022-12-15 01:18:19 +0900

    feat: update

--- 2022/baekjoon/OK/python/2530.py
+++ 2022/baekjoon/OK/python/2530.py
@@ -1,15 +1,17 @@
 import sys
 
 
+# Utilities ---------------------
 debug = False
-
 def p(name, value):
     if debug: print(f'{name} = {value}')
 def ip(): return sys.stdin.readline().strip()
 def ips(): return sys.stdin.readline().strip().split()
 def mv(type): return map(type, ips())
+# -------------------------------
 
 
+# Function Block ----------------
 # '시간', '분', '초'를 입력받아 '초'로 변경함
 def time_to_sec(h:int, m:int, s:int):
     xh = h * 60 * 60
@@ -26,16 +28,20 @@ def sec_to_time(s:int):
     s -= (mm * 60)
     ss = s
     return hh, mm, ss
+# -------------------------------
 
 
+# Please write the code below ---
 def main():
     h, m, s = mv(int)
     ts = int(ip())
     hh, mm, ss = sec_to_time(time_to_sec(h, m, s) + ts)
     print(f'{hh%24} {mm} {ss}')
+# -------------------------------
 
 
+# Ignore it ---------------------
 if __name__ == "__main__":
     main()
-
+# -------------------------------
 
