670e291ce567e3439afd7c992d189361cb35e9c3
chihiro888 <93immm@naver.com>
2023-05-31 17:32:44 +0900

    feat: solve 10799

--- 2023/boj/stack/10799.cpp
+++ 2023/boj/stack/10799.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
 using namespace std;
 
 int main() {
-    // TODO
-    printf("hello world");
+    string d;
+    cin >> d;
+    
+    stack<char> s;
+    int count = 0;
+
+    for (int i=0; i<d.size(); i++) {
+        char curr = d[i];
+        char prev = d[i-1];
+
+        if (curr == '(') {
+            s.push(curr);
+        } else {
+            if (prev == '(') {
+                s.pop();
+                count += s.size();
+            } else {
+                s.pop();
+                count += 1;
+            }
+        }
+    }
+
+    cout << count;
+
     return 0;
 }
