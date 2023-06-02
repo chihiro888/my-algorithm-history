9b07563b2285ba5c148f148e7ad38629b9bc91a9
chihiro888 <93immm@naver.com>
2023-06-02 23:56:07 +0900

    feat: solve 1406

--- 2023/boj/stack/1406.cpp
+++ 2023/boj/stack/1406.cpp
@@ -1,47 +1,78 @@
 #include <iostream>
 #include <string>
 #include <stack>
-#include <vector>
 
-#define FOR(i, start, end) for(int i = start; i < end; ++i)
-#define REP(i,n) FOR(i,0,n)
+#define FOR(i, start, end) for (int i = start; i < end; ++i)
+#define REP(i, n) FOR(i, 0, n)
 
 using namespace std;
 
-int main() {
-    string d;
-    int l;
-    cin >> d;
-    cin >> l;
+int main()
+{
+    // input
+    string input;
+    int input_size;
+    cin >> input;
+    cin >> input_size;
     cin.ignore();
 
-    int cursor = d.size();
+    // stack
+    stack<char> temp_stack;
+    stack<char> current_stack;
 
-    int i;
-    REP(i, l) {
+    REP(i, input.size())
+    {
+        current_stack.push(input[i]);
+    }
+
+    REP(i, input_size)
+    {
         string command;
         getline(cin, command);
 
         char opcode = command[0];
-
         char data = command[2];
-        string t = "";
-        t += data;
 
-        if (opcode == 'P') {
-            d.insert(cursor, t);
-            cursor++;
-        } else if (opcode == 'L' && cursor != 0) {
-            cursor--;
-        } else if (opcode == 'D' && cursor != d.size()) {
-            cursor++;
-        } else if (opcode == 'B' && cursor != 0) {
-            d.erase(cursor-1, 1);
-            cursor--;
+        if (opcode == 'P')
+        {
+            current_stack.push(data);
+        }
+        else if (opcode == 'B' && !current_stack.empty())
+        {
+            current_stack.pop();
+        }
+        else if (opcode == 'L' && !current_stack.empty())
+        {
+            temp_stack.push(current_stack.top());
+            current_stack.pop();
+        }
+        else if (opcode == 'D' && !temp_stack.empty())
+        {
+            current_stack.push(temp_stack.top());
+            temp_stack.pop();
         }
     }
 
-    cout << d << endl;
+    while (!temp_stack.empty())
+    {
+        current_stack.push(temp_stack.top());
+        temp_stack.pop();
+    }
+
+    // reverse print using stack
+    while (!current_stack.empty())
+    {
+        temp_stack.push(current_stack.top());
+        current_stack.pop();
+    }
+
+    while (!temp_stack.empty())
+    {
+        cout << temp_stack.top() << "";
+        temp_stack.pop();
+    }
+
+    cout << endl;
 
     return 0;
 }
