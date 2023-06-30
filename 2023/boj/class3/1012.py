4a722ec72be982608be1d96e00e4cb50756e5100
chihiro888 <93immm@naver.com>
2023-06-30 17:14:16 +0900

    feat: keep 1012

--- 2023/boj/class3/1012.py
+++ 2023/boj/class3/1012.py
@@ -1,5 +1,5 @@
 import sys
-
+sys.setrecursionlimit(10**6)
 
 # Utilities ---------------------
 def ip(): return sys.stdin.readline().strip()
@@ -16,18 +16,64 @@ def print_arr(arr):
             print(x, end='')
         print()
     print('----------------')
+
+def find_negative_one(arr):
+    for i, row in enumerate(arr):
+        for j, element in enumerate(row):
+            if element == 1:
+                return i, j
+    return -1, -1
+
+count = 0
+
+def dfs(g, m, n):
+    g[n][m] = 9
+
+    # 위
+    if n > 0 and g[n-1][m] == 1:
+        dfs(g, m, n-1)
+    # 아래
+    if n < len(g) - 1 and g[n+1][m] == 1:
+        dfs(g, m, n+1)
+    # 왼쪽
+    if m > 0 and g[n][m-1] == 1:
+        dfs(g, m-1, n)
+    # 오른쪽
+    if m < len(g[0]) - 1 and g[n][m+1] == 1:
+        dfs(g, m+1, n)
+
 # -------------------------------
 
 
 # Please write the code below ---
 def main():
     size = int(ip())
+
+    # m은 가로, n은 세로
     m, n, k = mv(int)
-    arr = [[0] * m for _ in range(n)]
+    g = [[0] * m for _ in range(n)]
+
     for _ in range(k):
-        x, y = mv(int)
-        arr[y][x] = 1
-    print_arr(arr)
+        # m은 가로, n은 세로
+        m, n = mv(int)
+        g[n][m] = 1
+
+    # print_arr(g)
+
+    count = 0
+    while True:
+        n, m = find_negative_one(g)
+
+        if n == -1 and m == -1:
+            break
+
+        dfs(g, m, n)
+
+        # print_arr(g)
+        
+        count += 1
+
+    print(count)
 # -------------------------------
 
 
