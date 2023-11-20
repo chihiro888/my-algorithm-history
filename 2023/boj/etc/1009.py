5940c0156db538c643f3980e183d6cd5861ac34e
chihiro888 <93immm@naver.com>
2023-11-20 16:42:02 +0900

    feat: update 1009

--- 2023/boj/etc/1009.py
+++ 2023/boj/etc/1009.py
@@ -10,10 +10,23 @@ def lmv(type): return list(map(type, ips()))
 
 
 # Function Block ----------------
-
+def op(a, b):
+    return int(str(a * b)[-1])
+    
+def solve(a, b):
+    q = []
+    q.append(op(a, a))
+    while q[-1] != a:
+        q.append(op(a, q[-1]))
+    q.insert(0, q.pop())
+    x = b % len(q)
+    print(q[x-1])
 # -------------------------------
 
 
 # Please write the code below ---
-
+t = int(ip())
+for i in range(t):
+    a, b = mv(int)
+    solve(a, b)
 # -------------------------------
