caa917e659d161dee88fcf45d3ca8f242a40cac9
chihiro888 <93immm@naver.com>
2024-01-02 13:30:49 +0900

    feat: update cpp template

--- note/default.cpp
+++ note/default.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
 #include <string>
 #include <stack>
 #include <vector>
 
 #define FOR(i, start, end) for (int i = start; i < end; ++i)
 #define REP(i, n) FOR(i, 0, n)
+#define FOREACH(i, container) for (auto i = container.begin(); i != container.end(); ++i)
+#define ALL(container) container.begin(), container.end()
 
 using namespace std;
 
